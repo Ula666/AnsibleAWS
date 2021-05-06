6f27bf664034e6d7ec0638f6aea63585ecd54725
rrose666 <uziolko@hotmail.com>
2021-05-06 17:04:40 +0100

    added new files

---
+++ provision.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# run the update command
+sudo apt-get update -y
+
+# upgrade
+sudo apt-get upgrade -y
+
+# install nginx
+sudo apt-get install nginx -y
+
+# install nodejs with required version and dependencies
+sudo apt-get install python-software-properties
+curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+sudo apt-get install nodejs -y
+
+# install npm with pm2 -g
+sudo npm install pm2 -g
+
+sudo npm install /home/vagrant/app/app
+sudo nodejs /home/vagrant/app/app/seeds/seed.js
+
+sudo echo "server {
+    listen 80;
+
+    server_name _;
+
+    location / {
+        proxy_pass http://localhost:3000;
+        proxy_http_version 1.1;
+        proxy_set_header Upgrade \$http_upgrade;
+        proxy_set_header Connection 'upgrade';
+        proxy_set_header Host \$host;
+        proxy_cache_bypass \$http_upgrade;
+    }
+}" | sudo tee /etc/nginx/sites-available/default
+
+
+sudo systemctl restart nginx
